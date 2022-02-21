4415468f2005d7ae4291d3652bd22b5e031d76f7
Wulfes <wolvenhollow@gmail.com>
2022-02-21 01:58:18 -0600

    added most of the setup for misc info, current weather, forecast

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 //ec48189d76f293875d59d78ec20af98b
 
+//weather data (left)
+const currentWeatherContainer = document.getElementById('currentWeatherContainer');
+const currentDescription = document.getElementById('description');
+const currentLocation = document.getElementById('location');
+const currentDate = document.getElementById('date');
+const currentTemp = document.getElementById('currentTemp');
+
+//bottom container
+const bottomContainer = document.getElementById('bottomContainer');
+const tabs = document.getElementById('tabsContainer');
+const hourlyTab = document.createElement('div');
+const dailyTab = document.createElement('div');
+
+//misc info (right)
+const miscContainer=document.getElementById('miscInfoContainer');
+const feelsLikeContainer = document.getElementById('feelsLikeContainer');
+const humidityContainer = document.getElementById('humidityContainer');
+const rainChanceContainer = document.getElementById('rainChanceContainer');
+const windSpeedContainer = document.getElementById('windSpeedContainer');
+
+let currentWeatherArray = [currentDescription, currentLocation, currentDate, currentTemp];
+let miscInfoDivArray = [[feelsLikeContainer], [humidityContainer], [rainChanceContainer], [windSpeedContainer]];
+
 class City {
     constructor(name, lat, lon, state, country){
         this.name = name;
@@ -10,9 +33,97 @@ class City {
     }
 }
 
+setupPage();
+
+function setupPage(){
+    //setup misc info (right side)
+    let divIconArray = [];
+    let divLabelArray = [];
+    let divMiscDataArray = [];
+
+    for (let i = 0; i<4; i++){
+        divIconArray[i] = document.createElement('i');
+        divLabelArray[i] = document.createElement('label');
+        divMiscDataArray[i] = document.createElement('div');
+
+        divIconArray[i].innerText = 'icon';
+        divLabelArray[i].innerText = 'label';
+        divMiscDataArray[i].innerText = 'data';
+
+        divIconArray[i].classList.add('icon');
+        divLabelArray[i].classList.add('miscLabel');
+        divMiscDataArray[i].classList.add('miscData');
+
+        miscInfoDivArray[i].push(divIconArray[i], divLabelArray[i], divMiscDataArray[i]);
+        miscInfoDivArray[i][0].append(divIconArray[i], divLabelArray[i], divMiscDataArray[i]);
+    }
+
+    tabs.append(dailyTab, hourlyTab);
+    dailyTab.innerText = 'Daily';
+    hourlyTab.innerText = 'Hourly';
+
+    dailyTab.classList.add('tab');
+    hourlyTab.classList.add('tab');
+
+    createForecastDivs();
+}
+
+function createForecastDivs(){
+    dailyArray = [];
+    hourlyArray = [];
+
+    const dailyContainer = document.createElement('div');
+    const hourlyContainer = document.createElement('div'); 
+
+    dailyContainer.classList.add('forecastContainer');
+    dailyContainer.classList.add('show');
+    hourlyContainer.classList.add('forecastContainer');
+
+    for (let i = 0; i<7; i++){
+        dailyArray[i] = document.createElement('div');
+        hourlyArray[i] = document.createElement('div');
+        
+        for (let j=0; j<4;j++){            
+            let newDiv = createNewSection(j);
+            dailyArray[i].append(newDiv);
+        }
+
+        dailyArray[i].classList.add('section');
+        hourlyArray[i].classList.add('section');
+
+        dailyContainer.append(dailyArray[i]);
+        hourlyContainer.append(hourlyArray[i]);
+    }
+    bottomContainer.append(dailyContainer, hourlyContainer);
+}
+
+function createNewSection(index){
+    let newDiv;
+    switch (index){
+        case 0:
+            newDiv = document.createElement('div');
+            newDiv.classList.add('forecastDay');
+            return newDiv;
+        case 1:
+            newDiv = document.createElement('div');
+            newDiv.classList.add('forecastTemp');
+            return newDiv;
+        case 2:
+            newDiv = document.createElement('div');
+            newDiv.classList.add('forecastTempLow');
+            return newDiv;
+        case 3:
+            newDiv = document.createElement('i');
+            newDiv.classList.add('forecastIcon');
+            return newDiv;
+    }    
+}
+
 let inputLocation = 'chicago';
 
-getLatLon(inputLocation);
+
+
+// getLatLon(inputLocation);
 
 async function getLatLon(inputLocation){
     let cityResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${inputLocation}&limit=5&appid=ec48189d76f293875d59d78ec20af98b`, {mode: 'cors'});
